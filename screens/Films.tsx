03bca6e0e5fa7514260c05c115b328ee55c1b9c4
Petar <petar.bozhidarov.todorov@gmail.com>
2020-07-26 18:04:51 +0200

    Added stack navigation

---
+++ screens/Films.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const Films: React.FC = () => {
+  return (
+    <View>
+      <Text>Films screen</Text>
+    </View>
+  );
+};
+
+export default Films;
---
+++ screens/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const Home: React.FC = () => {
+  return (
+    <View>
+      <Text>Home screen</Text>
+    </View>
+  );
+};
+
+export default Home;
